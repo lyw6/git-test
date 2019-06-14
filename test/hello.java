91e62e3df2382230cc882890c738ed6952df52d0
zhangsan <zhangsan@qq.com>
2019-06-15 06:51:44 +0800

    添加hello工程到git

--- test/hello.java
+++ test/hello.java
@@ -1,7 +1,8 @@
-package test;
+package it.heima.test;
 
 public class hello {
     public static void main(String[] args) {
+
         System.out.println("hello");
         System.out.println("hello");
         System.out.println("hello");
